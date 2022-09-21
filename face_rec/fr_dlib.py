c4c582deffb355d7ab161849db58bf1df76a90eb
Mert Terzi <113214682+merttrzi@users.noreply.github.com>
2022-09-21 18:09:41 +0300

    Add files via upload

---
+++ face_rec/fr_dlib.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import face_recognition
+# import moviepy.editor as mp
+
+# clip = mp.VideoFileClip("abc.mp4")
+# clip_resized = clip.resize(height = 640)
+
+# clip_resized.write_videofile("abc1.mp4")
+
+
+cap = cv2.VideoCapture("abc1.mp4")
+while True:
+    ret, frame = cap.read()
+
+    imgS = cv2.resize(frame,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+ 
+    facesCurFrame = face_recognition.face_locations(imgS)
+    for (y1, x2, y2, x1) in facesCurFrame:
+     
+        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
+
+
+    cv2.imshow("Frame",frame)
+    key =cv2.waitKey(1)
+    if key == 27:
+        break
+
+
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ face_rec/fr_opencv.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('cascade/data/haarcascade_frontalface_alt2.xml')
+
+cap = cv2.VideoCapture("abc1.mp4")
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# fourrc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter("fr_video.avi",fourrc,30,(width,height))
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor= 1.5, minNeighbors=5)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+        # w = width, h = height 
+       
+        color = (255,0,0)
+        thickness = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame,(x,y),(end_cord_x,end_cord_y),color,thickness)
+
+
+    cv2.imshow("Frame",frame)
+    key =cv2.waitKey(1)
+    if key == 27:
+        break
+
+
+
+cap.release()
+# out.release()
+cv2.destroyAllWindows()
+
+               
+
+
+
+
+
